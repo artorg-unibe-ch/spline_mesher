754a654e8da5b628e0436ca9ba0fbefbc5600cf9
Simone <simone.poncioni@gmail.com>
2022-10-20 19:37:21 +0200

    import matplotlib params from params.py

--- cortical_sanity.py
+++ cortical_sanity.py
@@ -1,19 +1,7 @@
 import numpy as np
 import math
-import matplotlib
 import matplotlib.pyplot as plt
-import seaborn as sns
 from scipy import spatial
-sns.set(font_scale=1.5)
-sns.set_style('white')
-
-# rcParams
-f = 1
-plt.rcParams['figure.figsize'] = [10 * f, 10 * f]
-plt.rcParams['font.size'] = 15
-
-matplotlib.rcParams['mathtext.fontset'] = 'stix'
-matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
 
 class CorticalSanity:
