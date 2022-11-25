c0d1817f9ac077a20dcdfd26713adc4d140842fc
Simone <simone.poncioni@gmail.com>
2022-11-25 15:33:48 +0100

    mv prototypes to avoid conflicts with nosetests

--- tests/tests_cortical_sanity.py
+++ tests/tests_cortical_sanity.py
@@ -1,15 +1,21 @@
-from spline_mesher import cortical_sanity as cs
+# from spline_mesher import cortical_sanity as cs
+from src.spline_mesher import cortical_sanity as cs
 import nose.tools as nt
 import numpy as np
+
 # Path: 02_CODE/tests/tests_cortical_sanity.py
+
 
 def test_ccw_angle():
     """CCW angle should be between 0 and 2*pi"""
     a = cs.CorticalSanityCheck
     a1 = np.random.rand(5, 2)
     a2 = np.random.rand(5, 2)
-    a = a.ccw_angle(a, array1=a1, array2=a2)
+    a = a.ccw_angle(
+        a, array1=a1, array2=a2, idx1=np.arange(len(a1)), idx2=np.arange(len(a2))
+    )
     nt.assert_true(0 <= a.all() <= 2 * np.pi)
+
 
 def test_convertRadiansToDegrees():
     """Convert to degrees should convert radians to degrees"""
@@ -17,41 +23,58 @@ def test_convertRadiansToDegrees():
     a = a.convertRadiansToDegrees(a, radians=np.pi)
     nt.assert_almost_equal(a, 180, places=7)
 
-def test_reset_numpy_index(arr=np.random.rand(100,2)):
+
+def test_reset_numpy_index(arr=np.random.rand(100, 2)):
     """Reset numpy index should reset the index to 0"""
     b = cs.CorticalSanityCheck
-    b = b.reset_numpy_index(b, arr, (len(arr[:,0])-1))
+    b = b.reset_numpy_index(b, arr, (len(arr[:, 0]) - 1))
     nt.assert_equal(b[0, 0], arr[-1, 0])
+
 
 def test_roll_numpy_index(arr=np.random.rand(100, 2)):
     c = cs.CorticalSanityCheck
     c = c.reset_numpy_index(c, arr, (len(arr[:, 0]) - 1))
 
     d = cs.CorticalSanityCheck
-    d = d.roll_index(d, arr, (len(arr[:, 0])-1))
+    d = d.roll_index(d, arr, (len(arr[:, 0]) - 1))
     nt.assert_equal(c[0, 0], d[0, 0])
+
 
 def test_is_angle_bigger_bool():
     alpha_int = np.array([15, 15, 15, 15])
     alpha_ext = np.array([1.5, 1.5, 1.5, 1.5])
 
     bool_angle = cs.CorticalSanityCheck
-    bool_angle = [bool_angle.is_angle_bigger_bool(bool_angle, alpha_int, alpha_ext) for alpha_int, alpha_ext in zip(alpha_int, alpha_ext)]
+    bool_angle = [
+        bool_angle.is_angle_bigger_bool(bool_angle, alpha_int, alpha_ext)
+        for alpha_int, alpha_ext in zip(alpha_int, alpha_ext)
+    ]
     print(bool_angle)
     nt.assert_true(np.asarray(bool_angle).all())
+
 
 def test_check_min_thickness():
     """Check intersections should return True if there are intersections"""
     a = cs.CorticalSanityCheck
-    a = a.check_min_thickness(a, np.full((5, 10), 1.00), np.full((5, 10), 1.01))
+    arr1 = np.full((5, 10), 1.00)
+    arr2 = np.full((5, 10), 1.01)
+    idx_arr1 = np.arange(len(arr1[:, 0]))
+    idx_arr2 = np.arange(len(arr2[:, 0]))
+    a = a.check_min_thickness(a, arr1, arr2, idx_arr1, idx_arr2)
     nt.assert_true(np.asarray(a).all())
+
 
 def test_check_bigger_thickness():
     b = cs.CorticalSanityCheck
-    b = b.check_min_thickness(b, np.full((5, 10), 1.00), np.full((5, 10), 10.00))
+    arr1 = np.full((5, 10), 1.00)
+    arr2 = np.full((5, 10), 10.00)
+    idx_arr1 = np.arange(len(arr1[:, 0]))
+    idx_arr2 = np.arange(len(arr2[:, 0]))
+    b = b.check_min_thickness(b, arr1, arr2, idx_arr1, idx_arr2)
     nt.assert_false(np.asarray(b).all())
 
-'''
+
+"""
 How to test cortical_sanity.py as a main file
 def test_main():
     ext_path = r'/home/simoneponcioni/Documents/01_PHD/03_Methods/Meshing/Meshing/code_pack/tmp/ext_surf_slice_minus1.npy'
@@ -64,4 +87,4 @@ def test_main():
                                   int_contour=int_spline)
     ext_spline, int_spline = cortex.tester()
     int_spline_corr = cortex.cortical_sanity_check(ext_spline, int_spline)
-'''
+"""
