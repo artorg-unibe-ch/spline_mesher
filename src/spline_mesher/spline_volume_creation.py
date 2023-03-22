f752a5b5450a907f4ddfcd3c1bbbebae9f48a21b
Simone <simone.poncioni@gmail.com>
2023-03-22 18:11:21 +0100

    debugging spline interpolation

--- src/spline_mesher/spline_volume_creation.py
+++ src/spline_mesher/spline_volume_creation.py
@@ -49,7 +49,6 @@ class OCC_volume:
         debug_orientation,
         show_plots,
         location,
-        offset,
         thickness_tol,
         phases,
     ):
@@ -67,7 +66,6 @@ class OCC_volume:
         self.debug_orientation = debug_orientation
         self.show_plots = bool(show_plots)
         self.location = str(location)
-        self.offset = int(offset)
 
         self.ASPECT = ASPECT
         self.SLICE = SLICE
@@ -441,8 +439,8 @@ class OCC_volume:
                 logging.warning("Something went wrong while flipping the array")
         return x_o, y_o
 
-    def sort_mahalanobis(self, data, metrics, start):
-        dist_m = ss.distance.squareform(ss.distance.pdist(data.T, metrics))
+    def sort_mahalanobis_legacy(self, data, start):
+        dist_m = ss.distance.squareform(ss.distance.pdist(data.T, "mahalanobis"))
         total_points = data.shape[1]
         points_index = set(range(total_points))
         sorted_index = []
@@ -461,6 +459,38 @@ class OCC_volume:
         x_mahalanobis = data[0][sorted_index]
         y_mahalanobis = data[1][sorted_index]
         return x_mahalanobis, y_mahalanobis
+
+    def remove_self_intersections(self, sorted_data):
+        pass
+
+    def sort_mahalanobis(self, data):
+        # Compute pairwise distances between all points in the contour
+        dist_matrix = ss.distance.cdist(data.T, data.T)
+
+        # Initialize variables
+        n_points = data.shape[1]
+        visited = np.zeros(n_points, dtype=bool)
+        sorted_indices = np.zeros(n_points, dtype=int)
+        sorted_indices[0] = 0
+        visited[0] = True
+
+        # Iterate over remaining points in the contour
+        for i in range(1, n_points):
+            # Compute distances from the previous point to all unvisited points
+            dist_to_prev = dist_matrix[sorted_indices[i - 1], :]
+            dist_to_prev[visited] = np.inf
+
+            # Find the closest unvisited point to the previous point
+            closest_unvisited = np.argmin(dist_to_prev)
+            sorted_indices[i] = closest_unvisited
+            visited[closest_unvisited] = True
+
+        # Reorder the data based on the sorted indices
+        sorted_data = data[:, sorted_indices]
+
+        # Ensure the contour does not self-intersect
+        # sorted_data = self.remove_self_intersections(sorted_data)
+        return sorted_data[0], sorted_data[1]
 
     def sort_surface(self, image_slice):
         """
@@ -484,15 +514,15 @@ class OCC_volume:
         x_s, y_s = self.sort_xy(x, y)
         xy_sorted = np.c_[x_s, y_s]
         xy_sorted_closed = np.vstack([xy_sorted, xy_sorted[0]])
-        x_mahalanobis, y_mahalanobis = self.sort_mahalanobis(
-            xy_sorted.T, "mahalanobis", 0
-        )
+        x_mahalanobis, y_mahalanobis = self.sort_mahalanobis(xy_sorted.T)
+
         x_mahalanobis = np.append(x_mahalanobis, x_mahalanobis[0])
         y_mahalanobis = np.append(y_mahalanobis, y_mahalanobis[0])
 
+        BNDS = 5
         # find the knot points
         tckp, u = splprep(
-            [x_mahalanobis, y_mahalanobis],
+            [x_mahalanobis[BNDS:-BNDS], y_mahalanobis[BNDS:-BNDS]],
             s=self.S,
             k=self.K,
             per=True,
@@ -507,10 +537,16 @@ class OCC_volume:
 
         # Sanity check to ensure directionality of sorting in cw- or ccw-direction
         xnew, ynew = self.check_orient(xnew, ynew, direction=1)
+        # return (
+        # xy_sorted_closed,
+        # xnew[1:],
+        # ynew[1:],
+        # )
+
         return (
             xy_sorted_closed,
-            xnew[1:],
-            ynew[1:],
+            xnew,
+            ynew,
         )
 
     def input_sanity_check(self, ext_contour_s: np.ndarray, int_contour_s: np.ndarray):
@@ -711,25 +747,35 @@ def main():
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         level=logging.DEBUG,
     )
-    logging.getLogger(os.getlogin())
-    logging.info("Starting meshing script...")
+    # logger = logging.getLogger(os.getlogin())
+    # logger.setLevel(logging.NOTSET)
+
+    logger = logging.getLogger(os.getlogin())
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    console_handler.setFormatter(logging.Formatter(log_format))
+    logger.addHandler(console_handler)
+
+    logger.info("Starting meshing script...")
     start = time.time()
 
     # fmt: off
-    img_basefilename = ["C0002234"]
+    img_basefilename = ["C0002233"]
     cwd = os.getcwd()
     img_basepath = f"{cwd}/01_AIM"
     img_outputpath = f"{cwd}/04_OUTPUT"
     img_path_ext = [str(Path(img_basepath, img_basefilename[i], img_basefilename[i] + "_CORT_MASK_cap.mhd",)) for i in range(len(img_basefilename))]
     filepath_ext = [str(Path(img_basepath, img_basefilename[i], img_basefilename[i])) for i in range(len(img_basefilename))]
     filename = [str(Path(img_outputpath, img_basefilename[i], img_basefilename[i] + "_ext.geo_unrolled")) for i in range(len(img_basefilename))]
-
+    # fmt: on
     geo_file_path = f"{cwd}/04_OUTPUT/C0002237/fake_example.geo_unrolled"
     mesh_file_path = f"{cwd}/04_OUTPUT/C0002237/C0002237.msh"
 
     for i in range(len(img_basefilename)):
-        Path.mkdir(Path(img_outputpath, img_basefilename[i]), parents=True, exist_ok=True)
-    # fmt: on
+        Path.mkdir(
+            Path(img_outputpath, img_basefilename[i]), parents=True, exist_ok=True
+        )
 
         cortical_v = OCC_volume(
             img_path_ext[i],
@@ -737,37 +783,47 @@ def main():
             filename[i],
             ASPECT=30,
             SLICE=1,
-            UNDERSAMPLING=5,
-            SLICING_COEFFICIENT=10, # 5, 10, 20 working on 2234
+            UNDERSAMPLING=1,
+            SLICING_COEFFICIENT=10,  # 5, 10, 20 working on 2234
             INSIDE_VAL=0,
             OUTSIDE_VAL=1,
             LOWER_THRESH=0,
             UPPER_THRESH=0.9,
             S=5,
             K=3,
-            INTERP_POINTS=50,
+            INTERP_POINTS=200,
             debug_orientation=0,
             show_plots=False,
             location="cort_ext",
-            offset=0,
-            thickness_tol=180e-3,
+            thickness_tol=180e-3,  # 180e-3,
             phases=2,
         )
 
         cortical_v.plot_mhd_slice()
         cortical_ext, cortical_int = cortical_v.volume_splines()
         # Cortical surface sanity check
-        cortex = csc.CorticalSanityCheck(MIN_THICKNESS=cortical_v.MIN_THICKNESS,
-                                         ext_contour=cortical_ext,
-                                         int_contour=cortical_int,
-                                         model=cortical_v.filename,
-                                         save_plot=False)
+        cortex = csc.CorticalSanityCheck(
+            MIN_THICKNESS=cortical_v.MIN_THICKNESS,
+            ext_contour=cortical_ext,
+            int_contour=cortical_int,
+            model=cortical_v.filename,
+            save_plot=False,
+        )
 
-        cortical_ext_split = np.array_split(cortical_ext, len(np.unique(cortical_ext[:, 2])))
-        cortical_int_split = np.array_split(cortical_int, len(np.unique(cortical_int[:, 2])))
+        cortical_ext_split = np.array_split(
+            cortical_ext, len(np.unique(cortical_ext[:, 2]))
+        )
+        cortical_int_split = np.array_split(
+            cortical_int, len(np.unique(cortical_int[:, 2]))
+        )
         cortical_int_sanity = np.zeros(np.shape(cortical_int_split))
         for i, _ in enumerate(cortical_ext_split):
-            cortical_int_sanity[i][:, :-1] = cortex.cortical_sanity_check(ext_contour=cortical_ext_split[i], int_contour=cortical_int_split[i], iterator=i, show_plots=False)
+            cortical_int_sanity[i][:, :-1] = cortex.cortical_sanity_check(
+                ext_contour=cortical_ext_split[i],
+                int_contour=cortical_int_split[i],
+                iterator=i,
+                show_plots=False,
+            )
             cortical_int_sanity[i][:, -1] = cortical_int_split[i][:, -1]
         cortical_int_sanity = cortical_int_sanity.reshape(-1, 3)
 
@@ -775,73 +831,199 @@ def main():
         gmsh.clear()
 
         N_TRANSVERSE = 3
-        N_RADIAL = 10
-        mesher = Mesher(geo_file_path, mesh_file_path, slicing_coefficient=cortical_v.SLICING_COEFFICIENT, n_transverse=N_TRANSVERSE, n_radial=N_RADIAL)
+        N_RADIAL = 15
+        mesher = Mesher(
+            geo_file_path,
+            mesh_file_path,
+            slicing_coefficient=cortical_v.SLICING_COEFFICIENT,
+            n_transverse=N_TRANSVERSE,
+            n_radial=N_RADIAL,
+            logger=logger,
+        )
 
-        cortex_centroid = np.zeros((len(cortical_ext_split), 3))  # center of mass of each slice (x, y, z)
-        cortical_int_sanity_split = np.array_split(cortical_int_sanity, len(np.unique(cortical_int_sanity[:, 2])))
+        cortex_centroid = np.zeros(
+            (len(cortical_ext_split), 3)
+        )  # center of mass of each slice (x, y, z)
+        cortical_int_sanity_split = np.array_split(
+            cortical_int_sanity, len(np.unique(cortical_int_sanity[:, 2]))
+        )
 
-        cortical_ext_centroid = np.zeros((np.shape(cortical_ext_split)[0], np.shape(cortical_ext_split)[1], np.shape(cortical_ext_split)[2]))
-        cortical_int_centroid = np.zeros((np.shape(cortical_int_split)[0], np.shape(cortical_int_split)[1], np.shape(cortical_int_split)[2]))
+        cortical_ext_centroid = np.zeros(
+            (
+                np.shape(cortical_ext_split)[0],
+                np.shape(cortical_ext_split)[1],
+                np.shape(cortical_ext_split)[2],
+            )
+        )
+        cortical_int_centroid = np.zeros(
+            (
+                np.shape(cortical_int_split)[0],
+                np.shape(cortical_int_split)[1],
+                np.shape(cortical_int_split)[2],
+            )
+        )
         idx_list_ext = np.zeros((len(cortical_ext_split), 4), dtype=int)
         idx_list_int = np.zeros((len(cortical_ext_split), 4), dtype=int)
         intersections_ext = np.zeros((len(cortical_ext_split), 2, 2, 3), dtype=float)
         intersections_int = np.zeros((len(cortical_ext_split), 2, 2, 3), dtype=float)
 
         for i, _ in enumerate(cortical_ext_split):
-            _, idx = np.unique(cortical_ext_split[i].round(decimals=6), axis=0, return_index=True)
+            _, idx = np.unique(
+                cortical_ext_split[i].round(decimals=6), axis=0, return_index=True
+            )
             cortical_ext_split[i][np.sort(idx)]
-            _, idx = np.unique(cortical_int_sanity_split[i].round(decimals=6), axis=0, return_index=True)
+            _, idx = np.unique(
+                cortical_int_sanity_split[i].round(decimals=6),
+                axis=0,
+                return_index=True,
+            )
             cortical_int_sanity_split[i][np.sort(idx)]
-            cortex_centroid[i][:-1] = mesher.polygon_tensor_of_inertia(cortical_ext_split[i], cortical_int_sanity_split[i])
+            cortex_centroid[i][:-1] = mesher.polygon_tensor_of_inertia(
+                cortical_ext_split[i], cortical_int_sanity_split[i]
+            )
             cortex_centroid[i][-1] = cortical_ext_split[i][0, -1]
-            cortical_ext_centroid[i], idx_list_ext[i], intersections_ext[i] = mesher.insert_tensor_of_inertia(cortical_ext_split[i], cortex_centroid[i][:-1])
-            cortical_int_centroid[i], idx_list_int[i], intersections_int[i] = mesher.insert_tensor_of_inertia(cortical_int_sanity_split[i], cortex_centroid[i][:-1])
+            (
+                cortical_ext_centroid[i],
+                idx_list_ext[i],
+                intersections_ext[i],
+            ) = mesher.insert_tensor_of_inertia(
+                cortical_ext_split[i], cortex_centroid[i][:-1]
+            )
+            (
+                cortical_int_centroid[i],
+                idx_list_int[i],
+                intersections_int[i],
+            ) = mesher.insert_tensor_of_inertia(
+                cortical_int_sanity_split[i], cortex_centroid[i][:-1]
+            )
 
         cortical_ext_msh = np.reshape(cortical_ext_centroid, (-1, 3))
         cortical_int_msh = np.reshape(cortical_int_centroid, (-1, 3))
 
-        indices_coi_ext, cortical_ext_bspline, intersection_line_tags_ext, cortical_ext_surfs = mesher.gmsh_geometry_formulation(cortical_ext_msh, idx_list_ext)
-        indices_coi_int, cortical_int_bspline, intersection_line_tags_int, cortical_int_surfs = mesher.gmsh_geometry_formulation(cortical_int_msh, idx_list_int)
-        intersurface_line_tags = mesher.add_interslice_segments(indices_coi_ext, indices_coi_int)
-        slices_tags = mesher.add_slice_surfaces(cortical_ext_bspline, cortical_int_bspline, intersurface_line_tags)
-        intersurface_surface_tags = mesher.add_intersurface_planes(intersurface_line_tags, intersection_line_tags_ext, intersection_line_tags_int)
+        (
+            indices_coi_ext,
+            cortical_ext_bspline,
+            intersection_line_tags_ext,
+            cortical_ext_surfs,
+        ) = mesher.gmsh_geometry_formulation(cortical_ext_msh, idx_list_ext)
+        (
+            indices_coi_int,
+            cortical_int_bspline,
+            intersection_line_tags_int,
+            cortical_int_surfs,
+        ) = mesher.gmsh_geometry_formulation(cortical_int_msh, idx_list_int)
+        intersurface_line_tags = mesher.add_interslice_segments(
+            indices_coi_ext, indices_coi_int
+        )
+        slices_tags = mesher.add_slice_surfaces(
+            cortical_ext_bspline, cortical_int_bspline, intersurface_line_tags
+        )
+        intersurface_surface_tags = mesher.add_intersurface_planes(
+            intersurface_line_tags,
+            intersection_line_tags_ext,
+            intersection_line_tags_int,
+        )
 
-        intersection_line_tags = np.append(intersection_line_tags_ext, intersection_line_tags_int)
+        intersection_line_tags = np.append(
+            intersection_line_tags_ext, intersection_line_tags_int
+        )
         cortical_bspline_tags = np.append(cortical_ext_bspline, cortical_int_bspline)
-        cortical_surfs = np.concatenate((cortical_ext_surfs, cortical_int_surfs, slices_tags, intersurface_surface_tags), axis=None)
+        cortical_surfs = np.concatenate(
+            (
+                cortical_ext_surfs,
+                cortical_int_surfs,
+                slices_tags,
+                intersurface_surface_tags,
+            ),
+            axis=None,
+        )
 
-        cort_vol_tags = mesher.add_volume(cortical_ext_surfs, cortical_int_surfs, slices_tags, intersurface_surface_tags)
+        cort_vol_tags = mesher.add_volume(
+            cortical_ext_surfs,
+            cortical_int_surfs,
+            slices_tags,
+            intersurface_surface_tags,
+        )
 
         # TODO: check if could be implemented when created (relationship with above functions)
         intersurface_line_tags = np.array(intersurface_line_tags, dtype=int).tolist()
 
         # add here trabecular meshing
-        trabecular_volume = TrabecularVolume(geo_file_path, mesh_file_path, slicing_coefficient=cortical_v.SLICING_COEFFICIENT, n_transverse=N_TRANSVERSE, n_radial=N_RADIAL)
+        trabecular_volume = TrabecularVolume(
+            geo_file_path,
+            mesh_file_path,
+            slicing_coefficient=cortical_v.SLICING_COEFFICIENT,
+            n_transverse=N_TRANSVERSE,
+            n_radial=N_RADIAL,
+            logger=logger,
+        )
         trabecular_volume.set_length_factor(0.4)
-        point_tags_r, trab_line_tags_v, trab_line_tags_h, trab_surfs_v, trab_surfs_h, trab_vols = trabecular_volume.get_trabecular_vol(coi_idx=intersections_int)
+        (
+            trab_point_tags,
+            trab_line_tags_v,
+            trab_line_tags_h,
+            trab_surfs_v,
+            trab_surfs_h,
+            trab_vols,
+        ) = trabecular_volume.get_trabecular_vol(coi_idx=intersections_int)
 
         # connection between inner trabecular and cortical volumes
-        trab_cort_line_tags = mesher.trabecular_cortical_connection(coi_idx=indices_coi_int, trab_point_tags=point_tags_r)
-        trab_slice_surf_tags = mesher.trabecular_slices(trab_cort_line_tags=trab_cort_line_tags, trab_line_tags_h=trab_line_tags_h, cort_int_bspline_tags=cortical_int_bspline)
+        trab_cort_line_tags = mesher.trabecular_cortical_connection(
+            coi_idx=indices_coi_int, trab_point_tags=trab_point_tags
+        )
+        trab_slice_surf_tags = mesher.trabecular_slices(
+            trab_cort_line_tags=trab_cort_line_tags,
+            trab_line_tags_h=trab_line_tags_h,
+            cort_int_bspline_tags=cortical_int_bspline,
+        )
 
-        trab_plane_inertia_tags = trabecular_volume.trabecular_planes_inertia(trab_cort_line_tags, trab_line_tags_v, intersection_line_tags_int)
-        cort_trab_vol_tags = mesher.get_trabecular_cortical_volume_mesh(trab_slice_surf_tags, trab_plane_inertia_tags, cortical_int_surfs, trab_surfs_v)
-
-        # meshing
-        trab_surfs = list(chain(trab_surfs_v, trab_surfs_h, trab_slice_surf_tags, trab_plane_inertia_tags))
-        trabecular_volume.meshing_transfinite(trab_line_tags_v, trab_line_tags_h, trab_surfs, trab_vols, trab_cort_line_tags)
-
+        trab_plane_inertia_tags = trabecular_volume.trabecular_planes_inertia(
+            trab_cort_line_tags, trab_line_tags_v, intersection_line_tags_int
+        )
+        cort_trab_vol_tags = mesher.get_trabecular_cortical_volume_mesh(
+            trab_slice_surf_tags,
+            trab_plane_inertia_tags,
+            cortical_int_surfs,
+            trab_surfs_v,
+        )
         volume_tags = np.concatenate((cort_vol_tags, cort_trab_vol_tags), axis=None)
+
+        # * meshing
+        trab_surfs = list(
+            chain(
+                trab_surfs_v,
+                trab_surfs_h,
+                trab_slice_surf_tags,
+                trab_plane_inertia_tags,
+            )
+        )
+        trabecular_volume.meshing_transfinite(
+            trab_line_tags_v,
+            trab_line_tags_h,
+            trab_surfs,
+            trab_vols,
+            trab_cort_line_tags,
+        )
 
         # * physical groups
         mesher.factory.synchronize()
         trab_vol_tags = np.concatenate((trab_vols, cort_trab_vol_tags), axis=None)
-        trab_physical_group = mesher.model.addPhysicalGroup(3, trab_vol_tags)
         cort_physical_group = mesher.model.addPhysicalGroup(3, cort_vol_tags)
+        trab_physical_group = mesher.model.addPhysicalGroup(3, trab_vol_tags)
 
-        mesher.meshing_transfinite(intersection_line_tags, cortical_bspline_tags, cortical_surfs, volume_tags, test_list=intersurface_line_tags)
+        print(
+            f"cortical physical group: {cort_physical_group}\ntrabecular physical group: {trab_physical_group}"
+        )
+
+        mesher.meshing_transfinite(
+            intersection_line_tags,
+            cortical_bspline_tags,
+            cortical_surfs,
+            volume_tags,
+            test_list=intersurface_line_tags,
+        )
         mesher.mesh_generate(dim=3)
+        mesher.analyse_mesh_quality()
 
         gmsh.fltk.run()
         gmsh.finalize()
