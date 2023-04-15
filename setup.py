fedce7cfc7e8db22c73c695457b7870524878ff4
Simone Poncioni <simone.poncioni@gmail.com>
2023-04-16 00:06:23 +0200

    Including cython code in source distributions

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 
 setup(
     name="spline_mesher",
-    packages=["spline_mesher"],
+    packages=["spline_mesher", "cython_functions"],
     ext_modules=cythonize("src/cython_functions/find_closed_curve.pyx"),
 )
