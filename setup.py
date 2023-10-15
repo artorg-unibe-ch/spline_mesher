c9c1bfd47ffc8f45a6c5df88edb1ef37fd7db8b5
Simone Poncioni <simone.poncioni@gmail.com>
2023-10-15 03:09:40 +0200

    new classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    version="0.0.1",
+    version="0.0.2",
     packages=["pyhexspline", "cython_functions"],
     ext_modules=cythonize("src/cython_functions/find_closed_curve.pyx"),
 )
