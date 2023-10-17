42ddda01c96a72adbc9b4bda77b891e98d1536e5
Simone Poncioni <simone.poncioni@gmail.com>
2023-10-17 19:21:40 +0200

    ellipsoid WIP

--- standalone.py
+++ standalone.py
@@ -51,12 +51,12 @@ def main():
         "thickness_tol": 5e-1,  # minimum cortical thickness tolerance: 3 * XCTII voxel size
         "phases": 2,  # 1: only external contour, 2: external and internal contour
         "center_square_length_factor": 0.5,  # size ratio of the refinement square: 0 < l_f < 1
-        # "n_elms_longitudinal": 4,  # number of elements in the longitudinal direction
-        # "n_elms_transverse_trab": 6,  # number of elements in the transverse direction for the trabecular compartment
-        # "n_elms_transverse_cort": 3,  # number of elements in the transverse direction for the cortical compartment
-        # "n_elms_radial": 15,  # number of elements in the radial direction # ! Should be 10 if trab_refinement is True
+        "n_elms_longitudinal": 4,  # number of elements in the longitudinal direction
+        "n_elms_transverse_trab": 6,  # number of elements in the transverse direction for the trabecular compartment
+        "n_elms_transverse_cort": 3,  # number of elements in the transverse direction for the cortical compartment
+        "n_elms_radial": 10,  # number of elements in the radial direction # ! Should be 10 if trab_refinement is True
         "mesh_order": 1,  # set order of the mesh (1: linear, 2: quadratic)
-        "show_plots": False,  # show plots during construction
+        "show_plots": True,  # show plots during construction
         "show_gmsh": True,  # show gmsh GUI
         "write_mesh": False,  # write mesh to file
         "trab_refinement": True,  # True: refine trabecular mesh at the center
@@ -73,50 +73,35 @@ def main():
 
     sitk_image_s = None
 
-    n_elms_longitudinal = np.linspace(2, 2, 5)
-    n_elms_transverse_trab = np.linspace(3, 3, 5)
-    n_elms_transverse_cort = np.linspace(2, 2, 5)
-    n_elms_radial = np.linspace(10, 10, 5)
-
-    for i, _ in enumerate(n_elms_longitudinal):
-        meshing_settings.update(
-            {
-                "n_elms_longitudinal": n_elms_longitudinal[i],
-                "n_elms_transverse_trab": n_elms_transverse_trab[i],
-                "n_elms_transverse_cort": n_elms_transverse_cort[i],
-                "n_elms_radial": n_elms_radial[i],
-            }
-        )
-
-        mesh = HexMesh(
-            meshing_settings,
-            img_settings,
-            sitk_image=sitk_image_s,
-            logger=logging.getLogger(LOGGING_NAME),
-        )
-        (
-            nodes,
-            elms,
-            centroids_cort,
-            centroids_trab,
-            elm_vol_cort,
-            elm_vol_trab,
-            radius_roi_cort,
-            radius_roi_trab,
-            bnds_bot,
-            bnds_top,
-            reference_point_coord,
-        ) = mesh.mesher()
-        print(f"Number of nodes: {len(nodes)}")
-        print(f"Number of elements: {len(elms)}")
-        print(f"Number of bottom boundary nodes: {len(bnds_bot)}")
-        print(f"Number of top boundary nodes: {len(bnds_top)}")
-        print(f"Number of centroids in trab physical group: {len(centroids_trab)}")
-        print(f"Number of centroids in cort physical group: {len(centroids_cort)}")
-        print(f"Radius ROI cort: {radius_roi_cort:.3f} (mm)")
-        print(f"Radius ROI trab: {radius_roi_trab:.3f} (mm)")
-        print(f"Reference point coordinates: {reference_point_coord} (mm)")
-        print("---------------")
+    mesh = HexMesh(
+        meshing_settings,
+        img_settings,
+        sitk_image=sitk_image_s,
+        logger=logging.getLogger(LOGGING_NAME),
+    )
+    (
+        nodes,
+        elms,
+        centroids_cort,
+        centroids_trab,
+        elm_vol_cort,
+        elm_vol_trab,
+        radius_roi_cort,
+        radius_roi_trab,
+        bnds_bot,
+        bnds_top,
+        reference_point_coord,
+    ) = mesh.mesher()
+    print(f"Number of nodes: {len(nodes)}")
+    print(f"Number of elements: {len(elms)}")
+    print(f"Number of bottom boundary nodes: {len(bnds_bot)}")
+    print(f"Number of top boundary nodes: {len(bnds_top)}")
+    print(f"Number of centroids in trab physical group: {len(centroids_trab)}")
+    print(f"Number of centroids in cort physical group: {len(centroids_cort)}")
+    print(f"Radius ROI cort: {radius_roi_cort:.3f} (mm)")
+    print(f"Radius ROI trab: {radius_roi_trab:.3f} (mm)")
+    print(f"Reference point coordinates: {reference_point_coord} (mm)")
+    print("---------------")
 
 
 if __name__ == "__main__":
