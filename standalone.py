c59c768c7f1f5a8835394df290da8ef1efee09eb
Simone Poncioni <simone.poncioni@gmail.com>
2024-08-26 18:05:44 +0200

    updated example fifgures

--- standalone.py
+++ standalone.py
@@ -51,7 +51,7 @@ def main():
         "interp_points": 1000,  # number of points to interpolate the spline
         "dp_simplification_outer": 5,  # Ramer-Douglas-Peucker simplification factor for the periosteal contour
         "dp_simplification_inner": 5,  # Ramer-Douglas-Peucker simplification factor for the endosteal contour
-        "thickness_tol": 5e-1,  # minimum cortical thickness tolerance: 3 * XCTII voxel size
+        "thickness_tol": 1,  # minimum cortical thickness tolerance: 3 * XCTII voxel size
         "phases": 2,  # 1: only external contour, 2: external and internal contour
         "center_square_length_factor": 0.4,  # size ratio of the refinement square: 0 < l_f < 1
         "mesh_order": 1,  # set element order (1: linear, 2: quadratic, >2: higher order, not tested)
@@ -61,7 +61,7 @@ def main():
         "n_elms_radial": 20,  # number of elements in the radial direction # ! Should be 10 if trab_refinement is True
         "ellipsoid_fitting": True,  # True: perform ellipsoid fitting in the inner trabecular compartment
         "show_plots": False,  # show plots during construction
-        "show_gmsh": True,  # show gmsh GUI
+        "show_gmsh": False,  # show gmsh GUI
         "write_mesh": True,  # write mesh to file
         "trab_refinement": False,  # True: refine trabecular mesh at the center (#! Should be set to False if ellipsoid fitting is True)
         "mesh_analysis": True,  # True: perform mesh analysis (plot JAC det in GMSH GUI)
